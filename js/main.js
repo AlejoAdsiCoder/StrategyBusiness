27510ce90c362e79e089aee57010fe4cfe322e4e
Alejandro Patino C <apatino@seedem.co>
2021-10-13 14:09:52 -0500

    creating the quiz

---
+++ js/main.js
@@ -0,0 +1,93 @@
+$(function () {
+    var pull = $('.nav-action');
+    menu = $('.principal');
+    menuHeight = menu.height();
+    social = $('.social');
+
+    $(pull).on('click', function (e) {
+        e.preventDefault();
+        menu.slideToggle();
+    });
+
+    $(window).resize(function () {
+        var w = $(window).width();
+        if (w > 992) {
+            menu.removeAttr('style');
+        }
+    });
+});
+
+
+$(function () {
+    $('.carousel-educacion').bxSlider({
+        slideWidth: 400,
+        minSlides: 1,
+        maxSlides: 3,
+        pager: false,
+    });
+});
+
+
+$(function () {
+    var submenu = $('nav li a');
+    $(submenu).on('click', function (e) {
+        //e.preventDefault();
+        $(this).parent().find('.submenu').toggleClass('visible');
+    });
+});
+
+
+$(function () {
+    $('a[href^="#"]').on('click', function (event) {
+
+        var target = $($(this).attr('href'));
+
+        if (target.length) {
+            //event.preventDefault();
+            $('html, body').animate({
+                scrollTop: target.offset().top
+            }, 500);
+        }
+
+    });
+});
+
+$(function () {
+    $('#video-gallery').royalSlider({
+        arrowsNav: false,
+        fadeinLoadedSlide: true,
+        controlNavigationSpacing: 0,
+        controlNavigation: 'thumbnails',
+
+        thumbs: {
+            autoCenter: false,
+            orientation: 'horizontal',
+            spacing: 4,
+            paddingBottom: 3
+        },
+        keyboardNavEnabled: true,
+        imageScaleMode: 'fill',
+        imageAlignCenter: false,
+        slidesSpacing: 0,
+        loop: false,
+        loopRewind: true,
+        numImagesToPreload: 3,
+        video: {
+            autoHideArrows: true,
+            autoHideControlNav: false,
+            autoHideBlocks: true
+        },
+        autoScaleSlider: true,
+        autoScaleSliderWidth: 960,
+
+
+        //news
+
+        autoHeight: true
+        //autoScaleSliderHeight: 750,
+
+        /* size of all images http://help.dimsemenov.com/kb/royalslider-jquery-plugin-faq/adding-width-and-height-properties-to-images */
+        //imgWidth: 640,
+        //imgHeight: 360
+    });
+});
---
+++ js/scripts.js
@@ -0,0 +1,60 @@
+// JavaScript Document
+
+ jQuery(document).ready(function($) {
+	 
+	 
+$('a[href^="#"]').on('click', function(event) {
+
+    var target = $( $(this).attr('href') );
+
+    if( target.length ) {
+        event.preventDefault();
+        $('html, body').animate({
+            scrollTop: target.offset().top
+        }, 500);
+    }
+
+});
+	
+	
+	$(".btn_menu").click(function(){
+		$(".container_header").slideToggle(500);
+	});
+	 
+		 
+  $('#video-gallery').royalSlider({
+    arrowsNav: false,
+    fadeinLoadedSlide: true,
+    controlNavigationSpacing: 0,
+    controlNavigation: 'thumbnails',
+
+    thumbs: {
+      autoCenter: false,
+      fitInViewport: true,
+      orientation: 'horizontal',
+      spacing: 0,
+      paddingBottom: 0
+    },
+    keyboardNavEnabled: true,
+    imageScaleMode: 'fill',
+    imageAlignCenter:true,
+    slidesSpacing: 0,
+    loop: false,
+    loopRewind: true,
+    numImagesToPreload: 3,
+    video: {
+      autoHideArrows:true,
+      autoHideControlNav:false,
+      autoHideBlocks: true
+    }, 
+    autoScaleSlider: true, 
+    autoScaleSliderWidth: 960,     
+    autoScaleSliderHeight: 750,
+
+    /* size of all images http://help.dimsemenov.com/kb/royalslider-jquery-plugin-faq/adding-width-and-height-properties-to-images */
+    imgWidth: 640,
+    imgHeight: 360
+
+  });
+});
+
